7a9004a0522124dbec91238154dcdd50bc6746c7
ryh <renyh@dp2003.com>
2021-03-26 17:43:54 +0800

    test

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace InventoryAPI
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
